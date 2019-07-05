09ac8b18bafab1e975d6ef4623332046756abdd2
Umesh <kushwaha4732@gmail.com>
2019-07-05 19:21:08 +0530

    Code refactor

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,6 @@
 class ContactsController < ApplicationController
+  before_action :find_contact, only: [:edit, :update, :destroy]
+
   def index
     if params[:group_id].present?
       @contacts = Group.find_by(id: params[:group_id]).contacts.order(created_at: "DESC").page(params[:page])    
@@ -15,34 +17,31 @@ class ContactsController < ApplicationController
     @contact = Contact.new(contact_params)
     if @contact.save
       flash[:success] = "Contact created successfully"
-      redirect_to contacts_index_path
+      redirect_to contacts_path
     else
       render 'new'
     end
   end
 
   def edit
-    @contact = Contact.find_by(id: params[:id])
   end
 
   def update
-    @contact = Contact.find_by(id: params[:id])
     if @contact.update(contact_params)
       flash[:success] = "Contact updated successfully"
-      redirect_to contacts_index_path
+      redirect_to contacts_path
     else
       render 'edit'
     end
   end
 
   def destroy
-    contact = Contact.find_by(id: params[:id])
-    if contact.destroy
+    if @contact.destroy
       flash[:success] = "Contact deleted successfully"
-      redirect_to contacts_index_path
+      redirect_to contacts_path
     else
       flash[:success] = "Something went wrong"
-      redirect_to contacts_index_path
+      redirect_to contacts_path
     end
   end
 
@@ -50,6 +49,10 @@ class ContactsController < ApplicationController
 
   def contact_params
     params.require(:contact).permit(:name, :company, :phone, :email, :address, :group_id, :avatar)
+  end
+
+  def find_contact
+    @contact = Contact.find_by(id: params[:id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   root 'contacts#index'
   # resources :contacts
-  get 'contacts/index'
-  get 'contacts/new', as: 'new_contact'
-  post 'contacts/create', as: 'create_contact'
-  get 'contacts/:id/edit', to: 'contacts#edit', as: 'edit_contact'
-  patch 'contacts/:id/update', to: 'contacts#update', as: 'update_contact'
-  delete 'contacts/:id/destroy', to: 'contacts#destroy', as: 'destroy_contact'
+  # get 'contacts/index'
+  # get 'contacts/new', as: 'new_contact'
+  # post 'contacts/create', as: 'create_contact'
+  # get 'contacts/:id/edit', to: 'contacts#edit', as: 'edit_contact'
+  # patch 'contacts/:id/update', to: 'contacts#update', as: 'update_contact'
+  # delete 'contacts/:id/destroy', to: 'contacts#destroy', as: 'destroy_contact'
+  resources 'contacts', except: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
